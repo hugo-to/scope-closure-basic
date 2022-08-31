8c0b4e6775c67fcfc5695d37c5def721d6fd9496
hugo-to <2hugoati@gmail.com>
2022-08-31 08:28:02 -0300

    primer

---
+++ src/scope/global.js
@@ -0,0 +1,16 @@
+// Variables 
+
+var a; //declarando
+var b = 'b'; // declaramos y asignamos
+b = 'bb'; //reasignamos
+var a = 'aa'; // redeclaracion
+
+// Global Scope
+
+var fruit = 'Apple'; // global
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
