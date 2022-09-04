57aefc88c939c7b5e235163386ce58dbe29451af
hugo-to <2hugoati@gmail.com>
2022-09-03 21:48:33 -0300

    8/11

---
+++ src/scope/re.js
@@ -0,0 +1,35 @@
+var firstName; // Undefined
+firstName = 'Hugo';
+console.log(firstName);
+
+var lastName = 'Alberto'; //declarar y asignar
+lastName = 'Anna'; // reasignar
+console.log(lastName);
+
+var secondName = 'Alberto'; // declarando y asignando
+var secondName = 'City'; // reasignando
+console.log(secondName);
+
+// Let
+
+let fruit = 'Apple' // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+
+
+// let fruit = 'Banana' ;  no se puede re declarar!!!
+
+// Const
+
+const animal = 'dog'; // declaro y asigno
+animal = 'cat'; // reasignado --------> no se puede reasignar en CONST
+// const animal = 'Snake'; // re declarando ----> no se puede re declarar tampoco!!!
+
+
+const vehicles = [];
+vehicles.push('🚗');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
+
+
